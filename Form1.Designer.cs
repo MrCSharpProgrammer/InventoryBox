1f890acb9dfe22e0f414db69816e04843c521f8a
Juan-Pierre <Juan-Pierre@DESKTOP-U98KHLS>
2023-10-10 23:55:50 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace InventoryBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.Searchbar = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.ListView1 = new System.Windows.Forms.ListView();
+            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Product_Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Stock = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ProductBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DescriptionBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PriceBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.StockBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.Question = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(616, 26);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(104, 36);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add Item";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(12, 76);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(115, 36);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "Remove Item";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // Searchbar
+            // 
+            this.Searchbar.Location = new System.Drawing.Point(470, 86);
+            this.Searchbar.Name = "Searchbar";
+            this.Searchbar.Size = new System.Drawing.Size(161, 26);
+            this.Searchbar.TabIndex = 2;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(637, 86);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(82, 29);
+            this.SearchButton.TabIndex = 3;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // ListView1
+            // 
+            this.ListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ID,
+            this.Product_Name,
+            this.Description,
+            this.Price,
+            this.Stock});
+            this.ListView1.FullRowSelect = true;
+            this.ListView1.HideSelection = false;
+            this.ListView1.Location = new System.Drawing.Point(12, 135);
+            this.ListView1.Name = "ListView1";
+            this.ListView1.Size = new System.Drawing.Size(707, 255);
+            this.ListView1.TabIndex = 4;
+            this.ListView1.UseCompatibleStateImageBehavior = false;
+            this.ListView1.View = System.Windows.Forms.View.Details;
+            this.ListView1.SelectedIndexChanged += new System.EventHandler(this.ListView1_SelectedIndexChanged);
+            // 
+            // ID
+            // 
+            this.ID.Text = "Product Name";
+            this.ID.Width = 120;
+            // 
+            // Product_Name
+            // 
+            this.Product_Name.Text = "ID";
+            // 
+            // Description
+            // 
+            this.Description.Text = "Description";
+            this.Description.Width = 100;
+            // 
+            // Price
+            // 
+            this.Price.Text = "Price";
+            // 
+            // Stock
+            // 
+            this.Stock.Text = "Stock";
+            // 
+            // IDBox
+            // 
+            this.IDBox.Location = new System.Drawing.Point(12, 32);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(110, 26);
+            this.IDBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Product Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // ProductBox
+            // 
+            this.ProductBox.Location = new System.Drawing.Point(133, 32);
+            this.ProductBox.Name = "ProductBox";
+            this.ProductBox.Size = new System.Drawing.Size(72, 26);
+            this.ProductBox.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ID";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // DescriptionBox
+            // 
+            this.DescriptionBox.Location = new System.Drawing.Point(212, 31);
+            this.DescriptionBox.Name = "DescriptionBox";
+            this.DescriptionBox.Size = new System.Drawing.Size(169, 26);
+            this.DescriptionBox.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(218, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Description";
+            // 
+            // PriceBox
+            // 
+            this.PriceBox.Location = new System.Drawing.Point(388, 32);
+            this.PriceBox.Name = "PriceBox";
+            this.PriceBox.Size = new System.Drawing.Size(100, 26);
+            this.PriceBox.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Price";
+            // 
+            // StockBox
+            // 
+            this.StockBox.Location = new System.Drawing.Point(495, 32);
+            this.StockBox.Name = "StockBox";
+            this.StockBox.Size = new System.Drawing.Size(100, 26);
+            this.StockBox.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(491, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Stock";
+            // 
+            // ClearButton
+            // 
+            this.ClearButton.Location = new System.Drawing.Point(133, 76);
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.Size = new System.Drawing.Size(115, 36);
+            this.ClearButton.TabIndex = 17;
+            this.ClearButton.Text = "Clear All";
+            this.ClearButton.UseVisualStyleBackColor = true;
+            this.ClearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // Question
+            // 
+            this.Question.Location = new System.Drawing.Point(255, 76);
+            this.Question.Name = "Question";
+            this.Question.Size = new System.Drawing.Size(42, 36);
+            this.Question.TabIndex = 18;
+            this.Question.Text = "?";
+            this.Question.UseVisualStyleBackColor = true;
+            this.Question.Click += new System.EventHandler(this.Question_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(989, 450);
+            this.Controls.Add(this.Question);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.StockBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PriceBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DescriptionBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ProductBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IDBox);
+            this.Controls.Add(this.ListView1);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.Searchbar);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Name = "Form1";
+            this.Text = "Inventory Box";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.TextBox Searchbar;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.ListView ListView1;
+        private System.Windows.Forms.ColumnHeader ID;
+        private System.Windows.Forms.ColumnHeader Product_Name;
+        private System.Windows.Forms.ColumnHeader Description;
+        private System.Windows.Forms.ColumnHeader Price;
+        private System.Windows.Forms.ColumnHeader Stock;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ProductBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DescriptionBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PriceBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox StockBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button Question;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryBox
+{
+    public partial class Form1 : Form
+    {
+        public string IDString;
+        public string ProductNameString;
+        public string ProductDescriptionString;
+        public string PriceString;
+        public string StockString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            //initializing strings
+            IDString = IDBox.Text.ToString();
+            ProductNameString = ProductBox.Text.ToString();
+            ProductDescriptionString = DescriptionBox.Text.ToString();
+            PriceString = PriceBox.Text.ToString();
+            StockString = StockBox.Text.ToString();
+
+            ListViewItem LVItem = new ListViewItem(IDString);
+            LVItem.SubItems.Add(ProductNameString);
+            LVItem.SubItems.Add(ProductDescriptionString);
+            LVItem.SubItems.Add(PriceString);
+            LVItem.SubItems.Add(StockString);
+            ListView1.Items.Add(LVItem);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Question_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Select the product in the list you want to remove and then click the remove item button, alternatively you can click the clear all buton to clear the list. ");
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            ListView1.Clear();
+        }
+
+        private void RemoveButton_Click(object sender, EventArgs e)
+        {
+            ListView1.Items.Remove(ListView1.SelectedItems[0]);
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            ListView1.SelectedItems.Clear();
+
+            for (int i = ListView1.Items.Count - 1; i >= 0; i--)
+            {
+                if (ListView1.Items[i].ToString().ToLower().Contains(Searchbar.Text.ToLower()))
+                {
+                    ListView1.Items[i].Selected = true;
+                    ListView1.Select();
+                }
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
